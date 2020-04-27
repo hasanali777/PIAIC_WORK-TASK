2e26826e17a959df6b131b153113e6dc2baf6e1c
hasanali777 <53380068+hasanali777@users.noreply.github.com>
2020-04-28 02:00:57 +0500

    Add files via upload

---
+++ piaic_asign1.py
@@ -0,0 +1,12 @@
+var1 = int(input("enter first no.: "))
+var2 = int(input("enter second no.: "))
+
+add = var1 + var2
+sub = var1 - var2
+mul = var1 * var2
+div = var1 / var2
+
+print("addition of two no's are: ",add)
+print("subtraction of two no's are: ",sub)
+print("multiplication of two no's are: ",mul)
+print("division of two no's are: ",div)
